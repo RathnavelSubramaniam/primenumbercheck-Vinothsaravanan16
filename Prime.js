aa51b10071f87058f295a73464984d04457a2c3b
Vinothsaravanan16 <vinothkd0707@gmail.com>
2026-07-02 12:34:36 +0530

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -8,12 +8,12 @@ const n = parseint (process. argv [2]);
 
 function isPrime(num) {
     if (num <= 1) return false;
-    for(let i=2; i<num; i++) 
+    for(let i=2;i<num; i++){ 
         if (num % i ==0) {
             return false;
         }
-}
-return true;
+    }
+    return true;
 }
 if (is prime(n) {
     console. log("prime number");
